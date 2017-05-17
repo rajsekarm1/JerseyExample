d4f85c3d1fb5ff819249c964be395cf882f8a603
rajsekarm <rajsekarm@gmail.com>
2017-05-18 00:32:01 +0530

    Initial Check-in

---
+++ JerseyEx/src/main/java/com/rajasekar/jerseyexample/Testmain.java
@@ -0,0 +1,13 @@
+package com.rajasekar.jerseyexample;
+
+public class Testmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
+
+
+
